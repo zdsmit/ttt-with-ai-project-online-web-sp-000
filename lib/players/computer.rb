bcad0cd61eb17f1cb8822e3332270963b90b8f81
Zach  Smith <zdsmith001@gmail.com>
2019-09-11 22:31:45 +0000

    Done.

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,26 +2,24 @@ module Players
   class Computer < Player
 
     def move(board)
-      if Game.player_1 == self || Game.player_2 == self
-        if board.cells[4] == !taken?
-          @board.update(board.cells[4], self)
-        elsif board.cells[0] == !taken?
-          @board.update(board.cells[0], self)
-        elsif board.cells[2] == !taken?
-          @board.update(board.cells[2], self)
-        elsif board.cells[6] == !taken?
-          @board.update(board.cells[6], self)
-        elsif board.cells[8] == !taken?
-          @board.update(board.cells[8], self)
-        elsif board.cells[1] == !taken?
-          @board.update(board.cells[1], self)
-        elsif board.cells[3] == !taken?
-          @board.update(board.cells[3], self)
-        elsif board.cells[5] == !taken?
-          @board.update(board.cells[5], self)
-        elsif board.cells[7] == !taken?
-          @board.update(board.cells[7], self)
-        end
+      if board.cells[4] == " "
+        "5"
+      elsif board.cells[0] == " "
+        "1"
+      elsif board.cells[2] == " "
+        "3"
+      elsif board.cells[6] == " "
+        "7"
+      elsif board.cells[8] == " "
+        "9"
+      elsif board.cells[1] == " "
+        "2"
+      elsif board.cells[3] == " "
+        "4"
+      elsif board.cells[5] == " "
+        "6"
+      elsif board.cells[7] == " "
+        "8"
       end
     end
 
