51c0383356655f46c53f591720cf1b852ac240e0
Zach  Smith <zdsmith001@gmail.com>
2019-09-11 15:57:02 +0000

    Done.

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,5 @@
 class Game
-  attr_accessor :board
+  attr_accessor :board, :player_1, :player_2
   WIN_COMBINATIONS = [
     [0, 1, 2],
     [3, 4, 5],
@@ -11,6 +11,51 @@ class Game
     [2, 4, 6]
   ]
 
+  def initialize(player_1 = Players::Human.new("X"), player_2 = Players::Human.new("O"), board = Board.new)
+    @player_1 = player_1
+    @player_2 = player_2
+    @board = board
+  end
 
+  def current_player
+    @board.turn_count.even? ? @player_1 : @player_2
+  end
+
+  def won?
+    WIN_COMBINATIONS.detect do |combination|
+      @board.cells[combination[0]] == @board.cells[combination[1]] &&
+       @board.cells[combination[1]] == @board.cells[combination[2]] &&
+       @board.taken?(combination[0]+1)
+    end
+  end
+
+  def draw?
+    if @board.full? && !won?
+      true
+    end
+  end
+
+  def over?
+    draw? || won?
+  end
+
+  def winner
+    if won?
+      @board.cells[won?.first]
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+    user_input = gets.strip
+    user_index = user_input.to_i - 1
+    if @board.valid_move?(user_index) && !@board.taken?(user_index)
+      @board.update(user_index, current_player)
+    end
+      @board.display
+  end
+
+  def play
+  end
 
 end
